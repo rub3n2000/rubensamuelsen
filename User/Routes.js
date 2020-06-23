87577fda4e32c320a4f3c2f3bfef2816cd2fdf51
rub3n2000 <ruben@tresam.no>
2020-06-23 13:12:40 +0200

    implemented auth, me route and made secrets less visible

---
+++ User/Routes.js
@@ -0,0 +1,81 @@
+// importing modules  
+const express = require('express');  
+const router = express.Router();  
+const passport = require("passport");
+const jwt = require('jsonwebtoken');
+const secrets = require('../secrets');
+const isLoggedIn = require('../middleware').isLoggedIn;
+
+// importing User Schema  
+const User = require('./Model');
+
+router.post('/register', function(req, res) { 
+  console.log(req.body);
+    Users=new User({username : req.body.username, isAdmin: false}); 
+
+        User.register(Users, req.body.password, function(err, user) { 
+        if (err) { 
+            res.json({success:false, 
+            message: "Your account could not be saved. " + err});
+        }else{ 
+            res.json({success: true, message: "Your account has been saved"}); 
+        } 
+    }); 
+});
+
+router.post('/login', function(req, res) { 
+    if(!req.body.username){ 
+      res.json({success: false, message: "Username was not given"}) 
+    } else { 
+      if(!req.body.password){ 
+        res.json({success: false, message: "Password was not given"}) 
+      }else{ 
+        passport.authenticate('local', function (err, user, info) {  
+           if(err){ 
+             res.json({success: false, message: err}) 
+             console.log(err);
+           } else{ 
+            if (!user) { 
+              res.json({success: false, message: 'username or password incorrect'}) 
+            } else{ 
+              req.login(user, function(err){ 
+                if(err){ 
+                  console.log(err);
+                  res.json({success: false, message: err}) 
+                }else{ 
+                  const token = jwt.sign({userId : user._id,  
+                     username:user.username}, secrets.authKey,  
+                        {expiresIn: '24h'}) 
+                  res.json({success:true, message:"Authentication successfull", token: token }); 
+                } 
+              }) 
+            } 
+           } 
+        })(req, res); 
+      } 
+    } 
+}); 
+
+router.get('/me', isLoggedIn, function(req,res) {
+  User.findById(req.user._id, function(err, user){
+    if(err) {
+      res.send(err);
+    }
+    else {
+      res.send(user);
+    }
+  });
+});
+
+router.get("/AmIAdmin", isLoggedIn, function(req,res) {
+  User.findById(req.user._id, function(err, user){
+    if(err) {
+      res.send(err);
+    }
+    else {
+      res.send({result: user.isAdmin});
+    }
+  });
+})
+
+module.exports = router; 
--- server.js
+++ server.js
@@ -1,24 +1,49 @@
+//#region  Imports
 const express = require('express');
 const path = require('path');
 const enforce = require('express-sslify');
+const mongoose = require('mongoose');
+const passport = require('passport');
+const bodyParser = require("body-parser");
+const User = require('./User/Model');
+const session = require("express-session");
+const LocalStrategy = require('passport-local').Strategy;  
+const userRoutes = require('./User/Routes');
+const secrets = require('./secrets');
 
 const app = express();
+mongoose.connect("mongodb+srv://rub3n2000:"+secrets.mongoPassword+"@rubensamuelsen-rjp7z.mongodb.net/rubensamuelsen?retryWrites=true&w=majority", 
+{ useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true});
+//#endregion
 
-app.use(enforce.HTTPS({ trustProtoHeader: true }))
+//#region  App setup statements
+//app.use(enforce.HTTPS({ trustProtoHeader: true }))
 app.use(express.static(path.join(__dirname, '/client/build')));
+app.use(require("express-session")({
+    secret: secrets.authKey,
+    resave: false,
+    saveUninitialized: false
+}));
+app.use(passport.initialize()); 
+app.use(passport.session()); 
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }))
+// parse application/json
+app.use(bodyParser.json())
+passport.serializeUser(User.serializeUser()); 
+passport.deserializeUser(User.deserializeUser()); 
+passport.use(new LocalStrategy(User.authenticate())); 
+app.use('/api/user', userRoutes);
+//#endregion
 
-app.get('/api/customers', (req,res) => {
-    const customers = [
-        {id: 1, firstName: 'Beth', lastName: 'Doe'},
-        {id: 2, firstName: 'John', lastName: 'Doe'}
-    ];
-    res.json(customers);
-});
-
+//#region Client Rendering Route
 app.get("*", (req, res) => {
-    res.sendFile('./index.html', {root: path.join(__dirname, '/client/build')});
+res.sendFile('./index.html', {root: path.join(__dirname, '/client/build')});
 });
+//#endregion
 
+//#region Server Starting
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Server started on port ${port}`));
+//#endregion 
