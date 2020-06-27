09c1dcc07704c248e0d88dff26cdfeb247af2f2b
Ruben Samuelsen <37822856+rub3n2000@users.noreply.github.com>
2020-06-27 03:27:36 +0200

    Update secrets.js

--- secrets.js
+++ secrets.js
@@ -2,3 +2,5 @@ module.exports = {
     authKey: "42134134123#_1232145123#sadasSDADW213d",
     mongoPassword: "QO8ybQENOGceQ63W"
 };
+
+//public github version just for demo purposes. Actual authkey and mongopassword is different changed after commit. And now it is gitignored.
