23df795391847dfc0419902dcc2765e152aedcd2
Ruben Samuelsen <37822856+rub3n2000@users.noreply.github.com>
2020-07-04 06:35:14 +0200

    Update secrets.js

--- secrets.js
+++ secrets.js
@@ -2,3 +2,5 @@ module.exports = {
     authKey: "dsadadadljkapd%¤!¤¤#%%DASD_AWD54357_ASDTYwrafghdysr&&",
     mongoPassword: "PW9RsVvkZsOJRV5N"
 };
+
+//not real secrets, just for demo purposes. Been changed since push.
