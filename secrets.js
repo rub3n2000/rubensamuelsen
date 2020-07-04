39c339845fc43b55bf6e04c7245c43a754399977
rub3n2000 <ruben@tresam.no>
2020-07-05 00:04:44 +0200

    changed secrets

--- secrets.js
+++ secrets.js
@@ -1,4 +1,4 @@
 module.exports = {
-  authKey: "dsadjgojp8uhij7ydaDWAD¤!321daw132%%&dadd_1235132DA21351%/Q¤!RA!123G_AD_",
-  mongoPassword: "eIxjCRQJ9vafQS2W",
+  authKey: "hafdsdwqedgggggggda414325RTDAS_!DASDA!¤5123",
+  mongoPassword: "T5Ae9ODrLHIaxNLu",
 };
