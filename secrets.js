1ddec95a39515e956abb7f04767e1141dff65e29
rub3n2000 <ruben@tresam.no>
2020-07-04 21:00:48 +0200

    new secrets

--- secrets.js
+++ secrets.js
@@ -2,5 +2,3 @@ module.exports = {
   authKey: "dsadjgojp8uhij7ydaDWAD¤!321daw132%%&dadd_1235132DA21351%/Q¤!RA!123G_AD_",
   mongoPassword: "eIxjCRQJ9vafQS2W",
 };
-
-//not real secrets, just for demo purposes. Been changed since push.
